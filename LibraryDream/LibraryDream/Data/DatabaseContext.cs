5fecbe40b92463a9888d6f1e59a472fc7262af2e
Oleksii Bosko <oleksii.bosko@oa.edu.ua>
2023-09-21 16:55:52 +0300

    Update DatabaseContext.cs

--- LibraryDream/LibraryDream/Data/DatabaseContext.cs
+++ LibraryDream/LibraryDream/Data/DatabaseContext.cs
@@ -1,6 +1,17 @@
-﻿namespace LibraryDream.Data
+﻿using LibraryDream.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryDream.Data
 {
-    public class DatabaseContext
+    public class DatabaseContext:DbContext
     {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Genre> Genre { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Book> Book { get; set;}
     }
 }
