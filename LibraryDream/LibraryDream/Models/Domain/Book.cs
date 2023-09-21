6d1f3982cf8b999e28eb11942321913cbd55f7f6
g0ras1ma <gorasima0@gmail.com>
2023-09-21 16:58:03 +0300

    Create model

--- LibraryDream/LibraryDream/Models/Domain/Book.cs
+++ LibraryDream/LibraryDream/Models/Domain/Book.cs
@@ -1,6 +1,23 @@
-﻿namespace LibraryDream.Models.Domain
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryDream.Models.Domain
 {
     public class Book
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Isbn { get; set; }
+        [Required]
+        public int TotalPages { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+        [Required]
+        public int GenreId { get; set; }
     }
 }
